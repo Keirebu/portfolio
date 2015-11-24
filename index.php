32b7c7658dc5117b1be6d1b820a04dc303ab22af
Keirebu <calebsensei@gmail.com>
2015-11-24 17:38:44 +0000

    intial commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    
+	<title>HTML5 boilerplate – all you really need…</title>
+	<link rel="stylesheet" href="scss/style.css">
+    <link rel="stylesheet" href="scss/slidebars.min.css">
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/countdown.js"></script>
+    
+    
+</head>
+
+<body>
+
+        <div class="video-container">
+
+            <video pre-load="auto" loop="true" muted="muted" autoplay="true" poster="">
+                <source src="horse.mp4" type="video/mp4" />
+                <source src="horse.webm" type="video/webm" />
+                <source src="horse.ogv" type="video/ogv" />
+            </video>
+
+            <div class="hero-content">
+                <img class="svg" src="images/idea-t.svg" alt="flat light bulb">
+                <p>It all starts with an idea</p>
+                <p class="tag-line">Portfolio of Caleb Stone a Nottingham based web designer.</p>
+
+            </div>
+
+            <div id="countdown">
+                
+                    <img class="cone" src="images/cone.svg" alt="flat traffic cone">
+                
+                    <p class="count-title">work underway</p>
+
+                    <div class="timer">
+                        <p class="days">00</p>
+                        <p class="timeRefDays">days</p>
+                        <p class="hours">00</p>
+                        <p class="timeRefHours">hours</p>
+                        <p class="minutes">00</p>
+                        <p class="timeRefMinutes">minutes</p>
+                        <p class="seconds">00</p>
+                        <p class="timeRefSeconds">seconds</p>
+                    </div>
+            
+            </div><!--end countdown-->
+            
+        </div><!--end video container-->
+    
+</body>
+</html>
